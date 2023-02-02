2a023b04b4e7401f74bfdd1adbca27915b3234b6
kxrn0 <kxrp@protonmail.com>
2023-02-02 10:18:13 -0600

    removed useless check

--- 6/unique_in_order/main.js
+++ 6/unique_in_order/main.js
@@ -1,8 +1,6 @@
 function uniqueInOrder(iterable) {
 	const parsed = [];
 
-	if (typeof iterable === "string") iterable = iterable.split("");
-
 	for (let char of iterable)
 		if (char !== parsed[parsed.length - 1]) parsed.push(char);
 	return parsed;
