0bc383fa61b0d7799270df7918781ed00adcad27
kxrn0 <kxrp@protonmail.com>
2023-02-02 12:48:35 -0600

    sudoku

--- 6/equal_sides_of_an_array/main.js
+++ 6/equal_sides_of_an_array/main.js
@@ -1,11 +1,9 @@
 function findEvenIndex(array) {
+	const sum = (total, current) => total + current;
+
 	for (let i = 0; i < array.length; i++) {
-		const left = array
-			.slice(0, i)
-			.reduce((total, current) => total + current, 0);
-		const right = array
-			.slice(i + 1)
-			.reduce((total, current) => total + current, 0);
+		const left = array.slice(0, i).reduce(sum, 0);
+		const right = array.slice(i + 1).reduce(sum, 0);
 
 		if (left === right) return i;
 	}
