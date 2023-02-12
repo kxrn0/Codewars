0f444a3245c5ded2d89679a95bca7c7b932cf30f
kxrn0 <kxrp@protonmail.com>
2023-02-12 10:56:01 -0600

    sum

---
+++ 6/two_sum/main.js
@@ -0,0 +1,11 @@
+function twoSum(numbers, target) {
+	const indices = [];
+
+	for (let i = 0; i < numbers.length - 1; i++)
+		for (let j = i + 1; j < numbers.length; j++)
+			if (numbers[i] + numbers[j] === target) return [i, j];
+}
+
+console.log(twoSum([1, 2, 3], 4));
+
+//time: 03mins, 04secs
